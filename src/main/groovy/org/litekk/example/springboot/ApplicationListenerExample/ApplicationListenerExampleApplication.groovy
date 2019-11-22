5de516076b78964f8b8f0d4bde0dd690edb128be
Seabra,El <seabrae@oclc.org>
2019-11-22 12:29:57 -0500

    Initial commit

---
+++ src/main/groovy/org/litekk/example/springboot/ApplicationListenerExample/ApplicationListenerExampleApplication.groovy
@@ -0,0 +1,13 @@
+package org.litekk.example.springboot.ApplicationListenerExample
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ApplicationListenerExampleApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ApplicationListenerExampleApplication, args)
+	}
+
+}
---
+++ src/test/groovy/org/litekk/example/springboot/ApplicationListenerExample/ApplicationListenerExampleApplicationTests.groovy
@@ -0,0 +1,13 @@
+package org.litekk.example.springboot.ApplicationListenerExample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationListenerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
